e87e73bc1148f59805d80d5f4d941234f12e9340
prince-myssie <prince.myssie@vocads.com>
2025-02-05 11:05:12 +0100

    fix: added home and posts pages

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,55 +1,18 @@
-import { useQuery } from '@apollo/client'
-import { Link } from 'react-router'
-import './App.css'
-import { graphql } from './gql'
+import React from "react";
+import Feeds from "./components/Feeds";
+import RightPanel from "./components/RightPanel";
+import Sidebar from "./components/SideBar";
 
-const getCharacters = graphql(`
-query GetCharacters($page: Int) {
-  characters(page: $page) {
-    info {
-      next
-    }
-    results {
-      id
-      name
-      image
-    }
-  }
-}  
-`)
-
-function App() {
-  const {data, error, loading, refetch} = useQuery(getCharacters)
-
-
-  console.log(data)
-  console.log(error);
-
-  if(loading) return <div>loading</div>
-
-  if (error || !data) return <div>error</div>
-
-  const onLoadMore = () => {
-    refetch({page: data.characters?.info?.next})
-  }
-
+const App: React.FC = () => {
   return (
-    <>
-      {data.characters?.info?.next && <button onClick={onLoadMore}>load more</button>}
-      <ul>
-        {(data.characters?.results ?? [])
-          .filter(character => character !== null)
-          .map(character => <li key={character.id}>
-            <Link to={`/${character.id}`}>
-              <div>
-                <img src={character.image ?? ''} alt={character.name ?? ''} />
-                <p>{character.name}</p>
-              </div>
-            </Link>
-          </li>)}
-      </ul>
-    </>
-  )
-}
+    <div className="flex bg-gradient-to-br from-gray-100 to-gray-300 min-h-screen min-w-screen">
+      <Sidebar />
+      <main className="flex-1 p-6">
+        <Feeds />
+      </main>
+      <RightPanel />
+    </div>
+  );
+};
 
-export default App
+export default App;
