d18abcee7dde42b9e1cdcd19dc0306d115b0d03e
MICLEA Christian <66925033+MicleaChristian@users.noreply.github.com>
2025-02-05 12:41:08 +0100

    fixed some small problems+ added login link if user is not logged in

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -12,22 +12,6 @@ const App: React.FC = () => {
         <Feeds />
       </main>
       <RightPanel />
-
-      <div>
-            <h1>Welcome to the Application</h1>
-            <p>Please navigate to one of the following options:</p>
-            <ul>
-                <li>
-                    <Link to="/login">Login</Link>
-                </li>
-                <li>
-                    <Link to="/login/register">Register</Link>
-                </li>
-                <li>
-                    <Link to="/login/reset">Reset Password</Link>
-                </li>
-            </ul>
-        </div>
     </div>
   );
 };
